541cba6f684d24ebae99ed2c527c5dfcbd76c9f3
hanonminione <hanonminione@users.noreply.github.com>
2018-03-29 16:30:11 +0200

    formulaires connexion et inscription

--- formConnexion.php
+++ formConnexion.php
@@ -4,27 +4,21 @@ include 'header.html';
 
 ?>
 
-Ici se trouve le formulaire de connexion.
+<h1>Connexion</h1>
 
 <form>
-  <fieldset>
-    <legend>Connexion</legend>
-    <div>
-    <div class="form-group">
-      <label for="mail">Identifiant</label>
+      <label for="mail">Identifiant : </label>
       <input type="email" id="mail" placeholder="ex.exemple@exemple.fr">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Mot de passe</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
-    <div class="form-check">
-        <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" value="" checked="">
-          Rester connecté
+      <br />
+      <label for="mdp">Mot de passe : </label>
+      <input type="password" id="mdp" placeholder="Mot de passe">
+    <div>
+        <label>
+          <input type="checkbox" id="ResterCo">Rester connecté
         </label>
-      </div>
-    <button type="submit" class="btn btn-primary">Connexion</button>
-  </fieldset>
+    </div>
+    <button type="submit">Connexion</button>
+    <a href="formInscription.php">Pas encore inscrit ?</a>
 </form>
 
 <?php
--- formInscription.php
+++ formInscription.php
@@ -4,7 +4,23 @@ include 'header.html';
 
 ?>
 
-Ici se trouve le formulaire d'inscription.
+<h1>Inscription</h1>
+
+<form>
+      <label for="mail">Adresse e-mail * : </label>
+      <input type="email" id="mail" placeholder="ex.exemple@exemple.fr">
+      <br />
+      <label for="mdp">Mot de passe * : </label>
+      <input type="password" id="mdp" placeholder="Mot de passe">
+      <br />
+      <label for="mdp">Répétez le mot de passe * : </label>
+      <input type="password" id="mdp" placeholder="Mot de passe">
+      <br />
+      <p>* Les champs avec une astérisque sont obligatoires !</p>
+    <div>
+    </div>
+    <button type="submit">S'inscrire</button>
+</form>
 
 <?php
 
