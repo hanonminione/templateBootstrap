af036788328dd49fd3df5291d76610e6c995cfc9
hanonminione <hanonminione@users.noreply.github.com>
2018-03-29 19:03:23 +0200

    contact

--- contact.php
+++ contact.php
@@ -7,16 +7,20 @@ include 'header.html';
 <h1>Contactez-nous</h1>
 
 <form>
+    <label for="sujet">Sujet : </label>
     <select name="sujet" id='sujet'>
-        <option>Vous êtes géniaux</option>
+        <option selected>Vous êtes géniaux</option>
         <option>Vous êtes beaux</option>
         <option>Vous êtes intelligents</option>
         <option>Je veux vous donner de l'argent</option>
         <option>Autre</option>
     </select>
+    <br /><br />
+    <label for="contenu">Message : </label>
     <textarea name="contenu" rows="10" cols="20">
 Tapez ici votre message...
     </textarea>
+    <br />
     <button type="submit">Envoyer</button>
     
 </form>
