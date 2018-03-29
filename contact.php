a23e44f602d2df6a37a3ce66eed24f0af0949ad1
Kensuo13 <kenmatsuo@hotmail.fr>
2018-03-29 19:07:52 +0200

    a

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@ include 'header.html';
 
 <h1>Contactez-nous</h1>
 
-<form>
+<form class="form">
+    
     <label for="sujet">Sujet : </label>
     <select name="sujet" id='sujet'>
         <option selected>Vous êtes géniaux</option>
