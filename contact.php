8c10026a10f4f583f7e02b3e172d51f8560ac3a4
hanonminione <hanonminione@users.noreply.github.com>
2018-03-29 19:47:44 +0200

    gallery

--- contact.php
+++ contact.php
@@ -7,23 +7,40 @@ include 'header.html';
 <h1>Contactez-nous</h1>
 
 <form class="form">
-    
-    <label for="sujet">Sujet : </label>
-    <select name="sujet" id='sujet'>
-        <option selected>Vous êtes géniaux</option>
-        <option>Vous êtes beaux</option>
-        <option>Vous êtes intelligents</option>
-        <option>Je veux vous donner de l'argent</option>
-        <option>Autre</option>
-    </select>
-    <br /><br />
-    <label for="contenu">Message : </label>
-    <textarea name="contenu" rows="10" cols="20">
-Tapez ici votre message...
-    </textarea>
-    <br />
-    <button type="submit">Envoyer</button>
-    
+    <table border="0">
+        <thead>
+            <tr>
+                <td>
+                    <label for="sujet">Sujet : </label>
+                </td>
+                <td>
+                    <select name="sujet" id='sujet'>
+                        <option selected>Vous êtes géniaux</option>
+                        <option>Vous êtes beaux</option>
+                        <option>Vous êtes intelligents</option>
+                        <option>Je veux vous donner de l'argent</option>
+                        <option>Autre</option>
+                    </select>
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <label for="contenu">Message : </label>
+                </td>
+                <td>
+                    <textarea name="contenu" rows="10" cols="20" id='contenu'></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="submit">Envoyer</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
 </form>
 
 <?php
--- gallery.php
+++ gallery.php
@@ -6,6 +6,16 @@ include 'header.html';
 
 <h1>Galerie</h1>
 
+<h1>Ceci est un h1</h1>
+<h2>Ceci est un h2</h2>
+<h3>Ceci est un h3</h3>
+<h4>Ceci est un h4</h4>
+<h5>Ceci est un h5</h5>
+<h6>Ceci est un h6</h6>
+<p>Ceci est un paragraphe</p>
+<i>Ceci est en italique</i> <br />
+<b>Ceci est en gras</b> <br />
+<a href="#">Ceci est un lien</a> <br />
 <?php
 
 include 'footer.html';
